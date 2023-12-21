e5fe76f5ef3787bc0f160935dd2ae81c2be837c3
Oscar Pacheco <oskarpacheco65@gmail.com>
2023-12-21 00:10:10 -0600

    cambio cors3

--- src/index.js
+++ src/index.js
@@ -10,9 +10,16 @@ import {PORT} from './config.js'
 
 const app = express();
 
-app.use(cors({
-    origin: ['https://playful-sunburst-bc2a70.netlify.app']
-})); 
+// app.use(cors({
+//     origin: ['https://playful-sunburst-bc2a70.netlify.app']
+// })); 
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next()
+})
 
 app.use(express.json());
 
